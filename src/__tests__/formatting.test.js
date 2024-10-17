f16e2fd72448a1414ecd8b724ab150ca83275285
Mle4nbly <mle4nbly@gmail.com>
2024-10-17 21:40:17 +0700

    Fix test and function

--- src/__tests__/formatting.test.js
+++ src/__tests__/formatting.test.js
@@ -1,10 +1,9 @@
 import clearingNumbers from "../js/formatting.js";
 
 test.each([
-    ['first number', '8 (927) 000-00-00', '+79270000000'],
-    ['second number', '+7 960 000 00 00', '+79600000000'],
-    ['third number', '+86 000 000 0000', '+860000000000']
-])('Test %s %s', (amount, expected) => {
-    const result = clearingNumbers(amount);
-    expect(clearingNumbers(result)).toBe(clearingNumbers(expected)); 
+    ['8 (927) 000-00-00', '+79270000000'],
+    ['+7 960 000 00 00', '+79600000000'],
+    ['+86 000 000 0000', '+860000000000']
+])('Test %s number', (amount, expected) => {
+    expect(expected).toBe(clearingNumbers(amount)); 
 });
--- src/js/formatting.js
+++ src/js/formatting.js
@@ -3,7 +3,7 @@ export default function clearingNumbers(number) {
 
     if (symbolClear.length == 11 && symbolClear[0] == 8) {
         return symbolClear.replace(/8/, '+7');
-    } else if (symbolClear.length > 11) {
+    } else {
         return '+' + symbolClear;
     };
 };
